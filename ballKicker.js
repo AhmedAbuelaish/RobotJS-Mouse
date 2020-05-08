3cff90917672a3305d2330ea028237089071e28c
Ahmed Rami Abuelaish <AAbuelaish@slb.com>
2020-05-08 07:22:22 -0500

    working timer

--- ballKicker.js
+++ ballKicker.js
@@ -8,15 +8,14 @@ var screenSize = robot.getScreenSize()
 var height = screenSize.height
 var width = screenSize.width
 var x = width / 2
-var y = 200 + (3 * height) / 4
+var y = (3 * height) / 4
 
-var initialStep = 3 // 3 = 280940m
-var finalPosition = 0 // if finalPosition is -ve, the game takes that as zero. (all -ve values = 0)
-
-holdBall()
+holdBall(220)
 setTimeout(kickBall, 1000)
+// setTimeout(kickBallBack, 1000)
 
-function holdBall() {
+function holdBall(offset) {
+	y+=offset
 	console.log('position mouse on ball x:', x, 'y:', y)
 	robot.moveMouse(x, y)
 	console.log('mouse down')
@@ -24,9 +23,22 @@ function holdBall() {
 }
 
 function kickBall() {
+	let initialStep = 3 // 3 = 280940m
+	let finalPosition = 0 // if finalPosition is -ve, the game takes that as zero. (all -ve values = 0)
 	y -= initialStep
 	robot.dragMouse(x, y)
 	y = finalPosition
+	robot.dragMouse(x, y)
+	console.log('mouse up x:', x, 'y:', y)
+	robot.mouseToggle('up')
+}
+
+function kickBallBack() {
+	let initialStep = -3 // 
+	let finalStep = -80 // 
+	y -= initialStep
+	robot.dragMouse(x, y)
+	y -= finalStep
 	robot.dragMouse(x, y)
 	console.log('mouse up x:', x, 'y:', y)
 	robot.mouseToggle('up')
--- screenSaver.js
+++ screenSaver.js
@@ -9,15 +9,39 @@ var screenSize = robot.getScreenSize()
 var height = screenSize.height / 2 - 10
 var width = screenSize.width
 var radius = Math.min(height/10 - 50,width/10 - 50)
+const startTime = new Date()
+var currentTime, runTime , hours, mins, secs
+var counter=0
 
 sineWaveCycle()
 // seedOfLifeCycle()
 
+function print(){
+	// Timer:
+	currentTime = new Date()
+	runTime = Math.round((currentTime - startTime)/1000) //in secs
+	hours = Math.floor(runTime /3600 % 24).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+	mins = Math.floor(runTime /60 % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+	secs = (runTime % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+	// Message:
+	console.clear()
+	console.log('')
+	console.log('---------------screenSaver.js is running---------------')
+	console.log ('start time:', startTime.toLocaleString())
+	console.log ('run time:', hours+':'+mins+':'+secs)
+	console.log('')
+	console.log('hit ctrl+c at any time to stop')
+}
+
 function sineWaveCycle() {
+	print()
+
 	for (var x = 0; x < width; x++) {
 		y = height * Math.sin((twoPI * x) / width) + height
 		robot.moveMouse(x, y)
 	}
+
+	print()
 
 	for (var x = width; x > 0; x--) {
 		y = -height * Math.sin((twoPI * x) / width) + height
@@ -26,14 +50,14 @@ function sineWaveCycle() {
 	sineWaveCycle()
 }
 
-function circleCycle() {
-	var x = width/2
-	var y = height/2
-	for (var i = 0; i <= twoPI; i += 0.01) {
-		// Convert polar coordinates to cartesian
-		x = x + radius * Math.cos(i)
-		y = y + radius * Math.sin(i)
-		robot.moveMouse(x, y)
-	}
-	circleCycle()
-}
+// function circleCycle() {
+// 	var x = width/2
+// 	var y = height/2
+// 	for (var i = 0; i <= twoPI; i += 0.01) {
+// 		// Convert polar coordinates to cartesian
+// 		x = x + radius * Math.cos(i)
+// 		y = y + radius * Math.sin(i)
+// 		robot.moveMouse(x, y)
+// 	}
+// 	circleCycle()
+// }
